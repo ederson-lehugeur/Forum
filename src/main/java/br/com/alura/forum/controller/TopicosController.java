aa4a52430e51b34ade23a2204342464aae2a7988
Éderson Lehugeur <eder.lehugeur@gmail.com>
2021-01-04 21:44:34 -0300

    Melhoria em TopicosController

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,19 +3,17 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
 
-@Controller
+@RestController
 public class TopicosController {
 
 	@GetMapping("/topicos")
-	@ResponseBody
-	public List<Topico> find() {
+	public List<Topico> findAll() {
 		Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
 
 		return Arrays.asList(topico, topico, topico);
