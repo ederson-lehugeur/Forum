bdde468c692d0a5a728b4d7b32f0675318675326
Éderson Lehugeur <eder.lehugeur@gmail.com>
2021-01-04 23:02:55 -0300

    Utilização do Spring Data JPA

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,23 +1,26 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.dto.TopicoDto;
-import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicosController {
 
+	@Autowired
+	TopicoRepository topicoRepository;
+
 	@GetMapping("/topicos")
 	public List<TopicoDto> findAll() {
-		Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
+		List<Topico> topicos = topicoRepository.findAll();
 
-		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+		return TopicoDto.converter(topicos);
 	}
 
 }
