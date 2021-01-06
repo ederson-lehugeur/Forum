2e502e84619e8acd06876a1db667acc8f95aa70d
Éderson Lehugeur <eder.lehugeur@gmail.com>
2021-01-05 23:36:09 -0300

    Cadastrando tópicos

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,33 +1,48 @@
 package br.com.alura.forum.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.alura.forum.dto.TopicoDto;
+import br.com.alura.forum.form.TopicoForm;
 import br.com.alura.forum.model.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
 
 	@Autowired
-	TopicoRepository topicoRepository;
+	private TopicoRepository topicoRepository;
 
-	/*@GetMapping("/topicos")
-	public List<TopicoDto> findAll() {
-		List<Topico> topicos = topicoRepository.findAll();
+	@Autowired
+	private CursoRepository cursoRepository;
 
-		return TopicoDto.converter(topicos);
-	}*/
-
-	@GetMapping("/topicos")
+	@GetMapping
 	public List<TopicoDto> find(String nomeCurso) {
 		List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
 
 		return TopicoDto.converter(topicos);
+	}
+
+	@PostMapping
+	public ResponseEntity<TopicoDto> save(@RequestBody TopicoForm topicoForm, UriComponentsBuilder uriBuilder) {
+		Topico topico = topicoForm.converter(cursoRepository);
+		topicoRepository.save(topico);
+
+		URI	uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+
+		return ResponseEntity.created(uri).body(new TopicoDto(topico));
 	}
 
 }
