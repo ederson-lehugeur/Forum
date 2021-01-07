20867350be3c42dfcd7e70e9e9691bfa9a13bae9
Éderson Lehugeur <eder.lehugeur@gmail.com>
2021-01-06 22:32:09 -0300

    Simplificação do JSON de exception

---
+++ src/main/java/br/com/alura/forum/dto/validation/ErroDeFormularioDto.java
@@ -0,0 +1,22 @@
+package br.com.alura.forum.dto.validation;
+
+public class ErroDeFormularioDto {
+
+	private String campo;
+	private String mensagem;
+
+	public ErroDeFormularioDto(String campo, String mensagem) {
+		super();
+		this.campo = campo;
+		this.mensagem = mensagem;
+	}
+
+	public String getCampo() {
+		return campo;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+}
