ae6718b06ca19a2807d3ca599e7b38365e4bbc5b
Éderson Lehugeur <eder.lehugeur@gmail.com>
2021-01-14 23:34:48 -0300

    Invalidando o cache

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -7,6 +7,7 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -71,6 +72,8 @@ public class TopicosController {
 	}
 
 	@PostMapping
+	@Transactional
+	@CacheEvict(value="listaDeTopicos", allEntries=true)
 	public ResponseEntity<TopicoDto> save(@RequestBody @Valid TopicoForm topicoForm,
 			UriComponentsBuilder uriBuilder) {
 
@@ -84,6 +87,7 @@ public class TopicosController {
 
 	@PutMapping("/{id}")
 	@Transactional
+	@CacheEvict(value="listaDeTopicos", allEntries=true)
 	public ResponseEntity<TopicoDto> update(@PathVariable Long id,
 			@RequestBody @Valid TopicoAtualizacaoForm topicoForm) {
 
@@ -99,6 +103,7 @@ public class TopicosController {
 
 	@DeleteMapping("/{id}")
 	@Transactional
+	@CacheEvict(value="listaDeTopicos", allEntries=true)
 	public ResponseEntity<?> delete(@PathVariable Long id) {
 		Optional<Topico> optional = topicoRepository.findById(id);
 
