eeacb85f3be131fe453b96c0482719e04e6d6486
Éderson Lehugeur <eder.lehugeur@gmail.com>
2021-01-23 22:28:57 -0300

    Testes automatizados com Spring Boot

---
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -0,0 +1,45 @@
+package br.com.alura.forum.repository;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+
+import br.com.alura.forum.model.Curso;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class CursoRepositoryTest {
+
+	@Autowired
+	private CursoRepository cursoRepository;
+
+	@Autowired
+	private TestEntityManager em;
+
+	@Test
+	void deveCarregarUmCursoPeloNomeBuscado() {
+		String nomeCurso = "HTML 5";
+
+		Curso html5 = new Curso();
+		html5.setNome(nomeCurso);
+		html5.setCategoria("Programacao");
+		em.persist(html5);
+
+		Curso curso = cursoRepository.findByNome(nomeCurso);
+		Assert.assertNotNull(curso);
+		Assert.assertEquals(nomeCurso, curso.getNome());
+	}
+
+	@Test
+	void naoDeveCarregarUmCursoCujoNomeNaoEstejaCadastrado() {
+		String nomeCurso = "JPA";
+		Curso curso = cursoRepository.findByNome(nomeCurso);
+		Assert.assertNull(curso);
+	}
+
+}
